c481acd05523577c897daca9a318e057d0fc26e2
alexlupu <alexcristi26@yahoo.com>
2023-12-20 11:40:38 +0100

    wievs and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -8,4 +7,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  resources :gardens do
+    resources :plants, only: :create
+  end
+  resources :plants, only: :destroy
+
 end
